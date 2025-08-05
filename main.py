50bb79fc1198be01b0d892cf856a016aa8744498
lisamial <72226240+lisamial@users.noreply.github.com>
2025-08-05 21:47:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = "8253855152:AAFm4wNtc5p12t3Y3TXGEaTcib2QUVs9KS0"
+BOT_TOKEN = os.environ.get("BOT_TOKEN_RW")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет! Я твой ЗОЖ-бот ✨")
